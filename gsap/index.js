a3433c2202b19f203a460324963bbff5555e754c
guna_9 <89364607+9una@users.noreply.github.com>
2022-03-29 20:23:23 +0900

    gsap edit

--- gsap/index.js
+++ gsap/index.js
@@ -1,57 +1,20 @@
-// gsap.registerPlugin(ScrollTrigger);
-
-// let sections = gsap.utils.toArray(".side-scrolling-wrapper .container .panel");
-
-// gsap.to(".side-scrolling-wrapper section", {
-//     xPercent: -100 * (sections.length - 3),
-//     scrollTrigger: {
-//         trigger: ".side-scrolling-wrapper",
-//         pin: true,
-//         markers: true,
-//         start: "top top",
-//         scrub: 1,
-//         end: () => "+=" + (document.querySelector(".wrap1 .container").offsetWidth / 5)
-//     }
-// });
-
-// gsap.to(".side-scrolling-wrapper2 section", {
-//     xPercent: -100 * (sections.length - 1),
-//     ease: "none",
-//     scrollTrigger: {
-//       trigger: ".side-scrolling-wrapper2",
-//       pin: true,
-//       start: "top +=10px",
-//       scrub: 1,
-//       end: () => "+=" + (document.querySelector(".container").offsetWidth / 5)
-//     }
-// });
-  
-// const container = document.querySelector(".wrap1 .container").offsetWidth / 5;
-// console.log("container : " + container);
-// console.log("xPercent : " + -100 * (sections.length - 1));
-// window.addEventListener('scroll', () => { 
-//     let y = window.scrollY;
-//     console.log(y);
-// })
-
-/* --------test1-------- */
 //fork from akapowl https://codepen.io/akapowl/pen/qBXOMme/5351d83e2b4241bfe3407ef6113e1f51
 
 gsap.registerPlugin(ScrollTrigger);
 
-const sections = gsap.utils.toArray(".horizontal__item");
-const sections2 = gsap.utils.toArray(".slide__item");
+const item = gsap.utils.toArray(".swiper-slide");
+const item2 = gsap.utils.toArray(".slide__item");
 let maxWidth = 0;
 
 const getMaxWidth = () => {
   maxWidth = 0;
-  sections.forEach(section => {
+  item.forEach(section => {
     maxWidth += section.offsetWidth;
     maxWidth += gsap.getProperty(section, 'marginLeft');
   });  
   
-  maxWidth += gsap.getProperty('.horizontal', 'paddingLeft')
-  maxWidth += gsap.getProperty('.horizontal__list', 'paddingLeft');
+  maxWidth += gsap.getProperty('.slide-box', 'paddingLeft')
+  maxWidth += gsap.getProperty('.swiper-wrapper', 'paddingLeft');
   // after the calculations above the right side of the last image  should be flush against the right side of the window
   // but with the scrollbar sitting on top of it, so you might want to add some space
   // if you want the white margin/padding to be visible
@@ -61,7 +24,7 @@ const getMaxWidth = () => {
 };
 const getMaxWidth2 = () => {
   maxWidth = 0;
-  sections2.forEach(section2 => {
+  item2.forEach(section2 => {
     maxWidth += section2.offsetWidth;
     maxWidth += gsap.getProperty(section2, 'marginLeft');
   });  
@@ -77,14 +40,15 @@ getMaxWidth();
 getMaxWidth2();
 ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
 
-gsap.set('.section1.spacer', { minHeight: window.innerHeight - document.querySelector('.horizontal').offsetHeight });
+gsap.set('.section1.spacer', { minHeight: window.innerHeight - document.querySelector('.slide-box').offsetHeight });
 gsap.set('.section2.spacer', { minHeight: window.innerHeight - document.querySelector('.slide').offsetHeight });
 
-gsap.to(sections, {
+/* 1 */
+gsap.to(item, {
   x: () => `-${maxWidth - window.innerWidth}`,
   ease: "none",
   scrollTrigger: {
-    trigger: ".horizontal",
+    trigger: ".slide-box",
     pin: '.wrapper',
     scrub: 0.5,
     markers: true,
@@ -93,7 +57,7 @@ gsap.to(sections, {
 
 
 
-sections.forEach((sct, i) => {
+item.forEach((sct, i) => {
   ScrollTrigger.create({
     trigger: sct,
     start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth / 2) * (maxWidth / (maxWidth - window.innerWidth)),
@@ -102,7 +66,8 @@ sections.forEach((sct, i) => {
 
 });
 
-gsap.to(sections2, {
+/* 2 */
+gsap.to(item2, {
   x: () => `-${maxWidth - window.innerWidth}`,
   ease: "none",
   scrollTrigger: {
@@ -113,7 +78,7 @@ gsap.to(sections2, {
     end: () => `+=${maxWidth}`,
     invalidateOnRefresh: true } });
 
-sections.forEach((sct, i) => {
+item.forEach((sct, i) => {
   ScrollTrigger.create({
     trigger: sct,
     start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth / 2) * (maxWidth / (maxWidth - window.innerWidth)),
